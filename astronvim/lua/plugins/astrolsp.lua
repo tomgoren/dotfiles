44c80c248d0e9da1d55c1ca70101d4ff19dcccb0
Tom Goren <tom@tomgoren.com>
2025-09-20 11:35:58 -0700

    Consolidate configs

--- astronvim/lua/plugins/astrolsp.lua
+++ astronvim/lua/plugins/astrolsp.lua
@@ -21,9 +21,7 @@ return {
       -- control auto formatting on save
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
+        allow_filetypes = { "go" },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
         },
