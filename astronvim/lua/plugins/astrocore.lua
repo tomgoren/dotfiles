63850b11dcf48f9230115606aebec442281d5b1d
Tom Goren <tgoren@stytch.com>
2025-09-23 11:57:56 -0700

    Disable neovim use global clipboard

--- astronvim/lua/plugins/astrocore.lua
+++ astronvim/lua/plugins/astrocore.lua
@@ -45,6 +45,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        clipboard = "",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
