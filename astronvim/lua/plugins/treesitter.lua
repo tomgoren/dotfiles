20f19ad61ec4215fe4901db6f1acbfd2a3353eb3
Tom Goren <tom.goren@gusto.com>
2026-04-08 14:14:23 -0700

    Update treesitter config

--- astronvim/lua/plugins/treesitter.lua
+++ astronvim/lua/plugins/treesitter.lua
@@ -4,21 +4,28 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "go",
-      "gomod",
-      "gosum",
-      "gowork",
-      "python",
-      "rust",
-      "yaml",
-      "bash",
-      "toml",
-      -- add more arguments for adding more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "python",
+        "rust",
+        "yaml",
+        "bash",
+        "toml",
+        -- add more arguments for adding more treesitter parsers
+      },
     },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
   },
 }
